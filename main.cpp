f674c964d0f702605077572de060038dbca4448e
Nikhil <nikhilljatt@gmail.com>
2025-10-31 14:00:21 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include "gamescreen.h"
+int main() {
+    
+    sf::RenderWindow window(sf::VideoMode(sf::VideoMode :: getDesktopMode()),  "The Game",sf::Style::Fullscreen);
+    window.setMouseCursorVisible(false);
+    window.setFramerateLimit(30);
+    renderGameScreen(window);
+    int screen = 0;
+    return 0;
+}
